a21d631d4ffbe478a0fdfaf3277b28baaf900fad
user <sairysss@gmail.com>
2023-07-03 14:16:12 +0200

    fixed lazy vim configs script

--- configs/neovim/lazy_vim/install.sh
+++ configs/neovim/lazy_vim/install.sh
@@ -4,13 +4,12 @@ CURRENT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "[neovim] creating sym links for lazy vim config..."
 
-mv ~/.config/nvim/config/lazy.lua ~/.config/nvim/config/lazy.lua.backup &&
-  ln -s $CURRENT_DIR/config/lazy.lua ~/.config/nvim/config/lazy.lua
+mv ~/.config/nvim/lua/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua.backup &&
+  ln -s $CURRENT_DIR/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua
 
-mv ~/.config/nvim/config/keymaps.lua ~/.config/nvim/config/keymaps.lua.backup &&
-  ln -s $CURRENT_DIR/config/keymaps.lua ~/.config/nvim/config/keymaps.lua
+mv ~/.config/nvim/lua/config/keymaps.lua ~/.config/nvim/lua/config/keymaps.lua.backup &&
+  ln -s $CURRENT_DIR/config/keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
 
-mv ~/.config/nvim/plugins/plugins.lua ~/.config/nvim/plugins/plugins.lua.backup &&
-  ln -s $CURRENT_DIR/plugins/plugins.lua ~/.config/nvim/plugins/plugins.lua
+ln -s $CURRENT_DIR/plugins/plugins.lua ~/.config/nvim/lua/plugins/plugins.lua
 
-ln -s $CURRENT_DIR/plugins/colorscheme.lua ~/.config/nvim/plugins/colorscheme.lua
+ln -s $CURRENT_DIR/plugins/colorscheme.lua ~/.config/nvim/lua/plugins/colorscheme.lua
